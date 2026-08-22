231b9779ea1eac228467a2499b55424104cc991e
DavidPerez3 <113512357+DavidPerez3@users.noreply.github.com>
2026-08-22 15:00:13 +0200

    Configure Supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
+import 'react-native-url-polyfill/auto';
+
+const supabaseUrl = 'https://uzdkzjdmaugkpxrpjbhs.supabase.co';
+const supabasePublishableKey = 'sb_publishable_81OK0pEF3o5z1XOa_Zy8-g_1fjeVA_g';
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: Platform.OS === 'web',
+  },
+});
